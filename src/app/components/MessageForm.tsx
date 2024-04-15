3a8c05e369b38b6aff4651e954c53d3f293386bb
DTA32 <97021335+DTA32@users.noreply.github.com>
2024-04-15 22:22:33 +0700

    message form now working!! and mobile dropdown fix

--- src/app/components/MessageForm.tsx
+++ src/app/components/MessageForm.tsx
@@ -6,13 +6,46 @@ export default function MessageForm() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
+    const [notification, setNotification] = useState("");
     const [loading, setLoading] = useState(false);
-    const handleSubmit = (e: any) => {
+    let hasSent = "";
+    if (typeof window !== "undefined") {
+        hasSent = localStorage.getItem("hasSent") || "";
+    }
+
+    async function handleSubmit(e: any) {
         e.preventDefault();
+        if (hasSent === "true" || hasSent !== "") {
+            setNotification("You have already sent a message.");
+            setTimeout(() => {
+                setNotification("");
+            }, 5000);
+            return;
+        }
         setLoading(true);
-        console.log(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
-        alert("Coming soon");
-    };
+        const result = await fetch("https://www.dta32.my.id/api/v2/message", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name: name, email: email, content: message }),
+        }).then((res) => res.json());
+        if (result.status === "success") {
+            localStorage.setItem("hasSent", "true");
+            setNotification("Message sent successfully.");
+            setName("");
+            setEmail("");
+            setMessage("");
+            setTimeout(() => {
+                setNotification("");
+            }, 5000);
+        } else {
+            console.error(result.message);
+            setNotification("Failed to send message.");
+            setTimeout(() => {
+                setNotification("");
+            }, 5000);
+        }
+        setLoading(false);
+    }
     return (
         <div className="flex flex-col gap-8 w-full px-4">
             <h1 className="text-2xl text-center">Leave a message</h1>
@@ -25,6 +58,7 @@ export default function MessageForm() {
                         onChange={(e) => setName(e.target.value)}
                         required
                         {...(loading && { disabled: true })}
+                        value={name}
                     />
                     <input
                         type="email"
@@ -33,6 +67,7 @@ export default function MessageForm() {
                         onChange={(e) => setEmail(e.target.value)}
                         required
                         {...(loading && { disabled: true })}
+                        value={email}
                     />
                 </div>
                 <textarea
@@ -43,14 +78,39 @@ export default function MessageForm() {
                     onChange={(e) => setMessage(e.target.value)}
                     required
                     {...(loading && { disabled: true })}
+                    value={message}
                 />
+                {notification && <p className="text-white">{notification}</p>}
                 <button
                     type="submit"
-                    className={`text-white p-4 rounded-2xl w-1/2 ${
+                    className={`flex items-center justify-center text-white p-4 rounded-2xl w-1/2 ${
                         loading ? "bg-slate-950" : "bg-slate-700 hover:bg-slate-600"
                     }`}
                     {...(loading && { disabled: true })}
                 >
+                    {loading && (
+                        // spinner from https://tailwindcss.com/docs/animation#spin
+                        <svg
+                            className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                        >
+                            <circle
+                                className="opacity-25"
+                                cx="12"
+                                cy="12"
+                                r="10"
+                                stroke="currentColor"
+                                strokeWidth="4"
+                            ></circle>
+                            <path
+                                className="opacity-75"
+                                fill="currentColor"
+                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                            ></path>
+                        </svg>
+                    )}
                     Send
                 </button>
             </form>
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,60 +5,63 @@ import Spotify from "./Spotify";
 export default function Navbar() {
     const [hamburger, setHamburger] = useState(false);
     return (
-        <nav className="bg-slate-950 flex flex-row justify-between px-8 h-[10vh] items-center sticky top-0 z-10 gap-x-20">
-            <div className="flex flex-row gap-x-0 md:gap-x-20 text-neutral-100 items-center w-full md:w-fit justify-between md:justify-normal">
-                <div className="flex md:hidden">
-                    <button className="relative" onClick={() => setHamburger(!hamburger)}>
-                        <div
-                            className={`relative flex overflow-hidden items-center justify-center rounded w-[50px] h-[50px] transform transition-all ring-gray-300 ${
-                                hamburger ? "ring-2" : "ring-0"
-                            } ring-opacity-30 duration-200 shadow-md`}
-                        >
-                            <div className="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden">
-                                <div
-                                    className={`bg-white h-[2px] w-7 transform transition-all duration-300 origin-left ${
-                                        hamburger ? "rotate-[42deg]" : "rotate-0"
-                                    }`}
-                                ></div>
-                                <div
-                                    className={`bg-white h-[2px] w-7 rounded transform transition-all duration-300 ${
-                                        hamburger ? "-translate-x-10" : "translate-x-0"
-                                    }`}
-                                ></div>
-                                <div
-                                    className={`bg-white h-[2px] w-7 transform transition-all duration-300 origin-left ${
-                                        hamburger ? "-rotate-[42deg]" : "rotate-0"
-                                    }`}
-                                ></div>
+        <nav className="flex flex-col h-[10vh] sticky top-0 z-10">
+            <div className="flex flex-row justify-between px-8 items-center gap-x-20 bg-slate-950 h-full">
+                <div className="flex flex-row gap-x-0 md:gap-x-20 text-neutral-100 items-center w-full md:w-fit justify-between md:justify-normal">
+                    <div className="flex md:hidden">
+                        <button className="relative" onClick={() => setHamburger(!hamburger)}>
+                            <div
+                                className={`relative flex overflow-hidden items-center justify-center rounded w-[50px] h-[50px] transform transition-all ring-gray-300 ${
+                                    hamburger ? "ring-2" : "ring-0"
+                                } ring-opacity-30 duration-200 shadow-md`}
+                            >
+                                <div className="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden">
+                                    <div
+                                        className={`bg-white h-[2px] w-7 transform transition-all duration-300 origin-left ${
+                                            hamburger ? "rotate-[42deg]" : "rotate-0"
+                                        }`}
+                                    ></div>
+                                    <div
+                                        className={`bg-white h-[2px] w-7 rounded transform transition-all duration-300 ${
+                                            hamburger ? "-translate-x-10" : "translate-x-0"
+                                        }`}
+                                    ></div>
+                                    <div
+                                        className={`bg-white h-[2px] w-7 transform transition-all duration-300 origin-left ${
+                                            hamburger ? "-rotate-[42deg]" : "rotate-0"
+                                        }`}
+                                    ></div>
+                                </div>
                             </div>
-                        </div>
-                    </button>
+                        </button>
+                    </div>
+                    <a
+                        href="#home"
+                        className="text-4xl select-none grow md:grow-0 text-center md:text-left pe-[50px] md:pe-0"
+                        onClick={() => setHamburger(false)}
+                    >
+                        MR
+                    </a>
+                    <div className="md:flex flex-row gap-x-10 text-2xl hidden">
+                        <a href="#home" className="linkHover">
+                            Home
+                        </a>
+                        <a href="#about" className="linkHover">
+                            About
+                        </a>
+                        <a href="#portfolio" className="linkHover">
+                            Portfolio
+                        </a>
+                        <a href="#contact" className="linkHover">
+                            Contact
+                        </a>
+                    </div>
                 </div>
-                <a
-                    href="#home"
-                    className="text-4xl select-none grow md:grow-0 text-center md:text-left pe-[50px] md:pe-0"
-                >
-                    MR
-                </a>
-                <div className="md:flex flex-row gap-x-10 text-2xl hidden">
-                    <a href="#home" className="linkHover">
-                        Home
-                    </a>
-                    <a href="#about" className="linkHover">
-                        About
-                    </a>
-                    <a href="#portfolio" className="linkHover">
-                        Portfolio
-                    </a>
-                    <a href="#contact" className="linkHover">
-                        Contact
-                    </a>
-                </div>
+                <Spotify />
             </div>
-            <Spotify />
             {hamburger && (
-                <div className="absolute md:hidden top-full transition-transform left-0 w-full h-screen text-neutral-100 animate-[scrollToBottom_1s]">
-                    <div className="flex flex-col text-2xl divide-y divide-slate-500 bg-slate-600 z-10 text-center">
+                <div className="absolute md:hidden top-full transition-transform left-0 w-full h-screen text-neutral-100 animate-[scrollToBottom_1s] z-[-5]">
+                    <div className="flex flex-col text-2xl divide-y divide-slate-500 bg-slate-600 z-[-5] text-center">
                         <a href="#home" className="w-full py-5" onClick={() => setHamburger(false)}>
                             Home
                         </a>
@@ -72,7 +75,10 @@ export default function Navbar() {
                             Contact
                         </a>
                     </div>
-                    <div className="top-0 w-full h-full z-[5]" onClick={() => setHamburger(false)}></div>
+                    <div
+                        className="top-0 w-full h-full z-[5] backdrop-blur-sm"
+                        onClick={() => setHamburger(false)}
+                    ></div>
                 </div>
             )}
         </nav>
