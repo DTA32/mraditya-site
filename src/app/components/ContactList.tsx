ea38f3d7d31f49c96d9f91aa738309002cd32c9b
DTA32 <97021335+dta32@users.noreply.github.com>
2025-06-27 00:43:13 +0700

    feat(contact): add yapping and reorder contact card

--- src/app/components/ContactList.tsx
+++ src/app/components/ContactList.tsx
@@ -5,7 +5,12 @@ import { IconName } from "@fortawesome/fontawesome-svg-core";
 export default function ContactList() {
     return (
         <div className="flex flex-col gap-8 w-full">
-            <h1 className="text-4xl text-center">Contact</h1>
+            <div className="text-center flex flex-col gap-4">
+                <h3 className="text-4xl">Contact</h3>
+                <div className="w-full flex justify-center">
+                    <p className="max-w-sm">If you have any questions, or interested to collaborate with me, can reach me out via these</p>
+                </div>
+            </div>
             <div className="flex flex-col gap-4">
                 <div className="flex flex-wrap justify-center gap-x-16 gap-y-2">
                     {ContactsJSON.map((contact) => (
