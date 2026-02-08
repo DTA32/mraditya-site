93c7935dcb19f641c303b353ba540716644238da
DTA32 <97021335+dta32@users.noreply.github.com>
2026-02-09 01:02:39 +0700

    feat: update now playing widget

--- next.config.js
+++ next.config.js
@@ -1,24 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "i.scdn.co",
-                pathname: "/image/**",
-            },
-            {
-                protocol: "https",
-                hostname: "cdn.jsdelivr.net",
-                pathname: "/gh/devicons/devicon/icons/**",
-            },
-            {
-                protocol: "https",
-                hostname: "assets.mraditya.my.id",
-                pathname: "/**",
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+        pathname: "/image/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.jsdelivr.net",
+        pathname: "/gh/devicons/devicon/icons/**",
+      },
+      {
+        protocol: "https",
+        hostname: "assets.mraditya.my.id",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lastfm.freetls.fastly.net",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,55 +1,56 @@
 "use client";
-import Spotify from "./Spotify";
+import NowPlaying from "./NowPlaying";
 import Image from "next/image";
-import {useScrollPosition} from "@/hooks/useScrollPosition";
+import { useScrollPosition } from "@/hooks/useScrollPosition";
 import Link from "next/link";
 
 export default function Navbar() {
-    const {scrollY, activeSection} = useScrollPosition();
-    const isScrolled = scrollY > 50;
+  const { scrollY, activeSection } = useScrollPosition();
+  const isScrolled = scrollY > 50;
 
-    return (
-        <nav className="flex flex-col h-[8vh] static md:sticky top-0 z-10">
-            <div className={`flex flex-row justify-between px-8 items-center h-full transition-all duration-300 ${
-                isScrolled
-                    ? 'bg-slate-950/50 backdrop-blur-md'
-                    : 'bg-slate-900 md:bg-slate-950'
-            }`}>
-                <div
-                    className="flex flex-row gap-x-0 md:gap-x-20 text-neutral-100 items-center w-full md:w-fit justify-center md:justify-normal">
-                    <Link href="/">
-                        <Image
-                            src="https://assets.mraditya.my.id/robot.png"
-                            alt="robot picture"
-                            width={40}
-                            height={40}
-                            className="w-10 h-full"
-                            loading="eager"
-                        />
-                    </Link>
-                    <div className="md:flex flex-row gap-x-10 text-xl hidden">
-                        <Link
-                            href="/about"
-                            className={`linkHover ${activeSection === 'about' ? 'active' : ''}`}
-                        >
-                            About
-                        </Link>
-                        <Link
-                            href="/portfolio"
-                            className={`linkHover ${activeSection === 'portfolio' ? 'active' : ''}`}
-                        >
-                            Portfolio
-                        </Link>
-                        <Link
-                            href="/contact"
-                            className={`linkHover ${activeSection === 'contact' ? 'active' : ''}`}
-                        >
-                            Contact
-                        </Link>
-                    </div>
-                </div>
-                <Spotify/>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="flex flex-col h-[8vh] static md:sticky top-0 z-10">
+      <div
+        className={`flex flex-row justify-between px-8 items-center h-full transition-all duration-300 ${
+          isScrolled
+            ? "bg-slate-950/50 backdrop-blur-md"
+            : "bg-slate-900 md:bg-slate-950"
+        }`}
+      >
+        <div className="flex flex-row gap-x-0 md:gap-x-20 text-neutral-100 items-center w-full md:w-fit justify-center md:justify-normal">
+          <Link href="/">
+            <Image
+              src="https://assets.mraditya.my.id/robot.png"
+              alt="robot picture"
+              width={40}
+              height={40}
+              className="w-10 h-full"
+              loading="eager"
+            />
+          </Link>
+          <div className="md:flex flex-row gap-x-10 text-xl hidden">
+            <Link
+              href="/about"
+              className={`linkHover ${activeSection === "about" ? "active" : ""}`}
+            >
+              About
+            </Link>
+            <Link
+              href="/portfolio"
+              className={`linkHover ${activeSection === "portfolio" ? "active" : ""}`}
+            >
+              Portfolio
+            </Link>
+            <Link
+              href="/contact"
+              className={`linkHover ${activeSection === "contact" ? "active" : ""}`}
+            >
+              Contact
+            </Link>
+          </div>
+        </div>
+        <NowPlaying />
+      </div>
+    </nav>
+  );
 }
